c25be5fcd2170d0e83e19cad927754ea9f9084e9
GuxtaHAB <gustavoberalto05@gmail.com>
2025-07-31 18:07:38 -0300

    Atualização dos arquivos

--- 01 -Classes e Objetos/src/Caneta.java
+++ 01 -Classes e Objetos/src/Caneta.java
@@ -12,7 +12,11 @@ public class Caneta {
         System.out.println("Está tampada? "+ this.tampada);
     }
     void rabiscar(){
-
+        if(this.tampada == true){
+            System.out.println("ERRO! Não posso rabiscar");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
     }
 
     void tampar(){
